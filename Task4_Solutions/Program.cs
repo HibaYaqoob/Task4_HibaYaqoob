0d9c488cab14d5344dd95871592286148be3485e
HibaYaqoob <16j2021906@utas.edu.om>
2026-07-06 23:17:46 +0400

    Task 1 - Personalized Welcome Function

--- Task4_Solutions/Program.cs
+++ Task4_Solutions/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1 - Personalized Welcome Function
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            PrintWelcome(name);
+        }
+
+        public static void PrintWelcome(string name)
+        {
+            // Function of task 1:
+            Console.WriteLine($"Welcome, "+name+" we are glad to have you here.");
         }
     }
 }
